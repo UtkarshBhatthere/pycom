bb1ac729c08345f9651dd9b234489a8742ee7b95
Utkarsh Bhatt <utkarsh_Bhatt@outlook.com>
2019-10-07 15:42:03 -0800

    Shifted to complex S param values using mag + rad

--- pycom.py
+++ pycom.py
@@ -1,16 +1,21 @@
-from numpy import imag, real, conj, exp, abs, log10, loadtxt, pi
+from numpy import imag, real, conj, abs, log10, loadtxt, savetxt, absolute
+import numpy
 import matplotlib.pyplot as plt
-from cmath import sqrt, log
+from cmath import sqrt, log, exp, pi, rect, e
 class pycom:
 
     #Funtional Data Members.
     path            = str()
     method_token    = dict()
 
+    #Storage Data members
+    s11 = list()
+    s12 = list()
+    s21 = list()
+    s22 = list()
+
     #Computational Data Members.
-    denominator = sqrt(2)
-    unit_theta  = pi/12 # 15 degrees.
-    theta_list  = [pi/12*i for i in range(1, 10)]
+    theta_list  = [pi/12*i for i in range(1, 10)] # 15 degrees.
 
 
     # Output Variables.
@@ -26,20 +31,27 @@ class pycom:
     def __init__(self, path_str: str):
         ''' Initialiser for the pycom Class, does all the lazy work preparation.'''
 
-        #Fetching the CSV File data to internal variables. Skip rows skips the Column title.
-        self.S11 = loadtxt(open(path_str+"S11.csv", "rb"), delimiter=",", skiprows=1)
-        self.S12 = loadtxt(open(path_str+"S12.csv", "rb"), delimiter=",", skiprows=1)
-        self.S21 = loadtxt(open(path_str+"S21.csv", "rb"), delimiter=",", skiprows=1)
-        self.S22 = loadtxt(open(path_str+"S22.csv", "rb"), delimiter=",", skiprows=1)
+        #Fetching the Real S params to internal variables. Skip rows skips the Column title.
+        self.S11_mag = loadtxt(open(path_str+"S11_mag.csv", "rb"), delimiter=",", skiprows=1)
+        self.S12_mag = loadtxt(open(path_str+"S12_mag.csv", "rb"), delimiter=",", skiprows=1)
+        self.S21_mag = loadtxt(open(path_str+"S21_mag.csv", "rb"), delimiter=",", skiprows=1)
+        self.S22_mag = loadtxt(open(path_str+"S22_mag.csv", "rb"), delimiter=",", skiprows=1)
+
+        #Fetching the Imag S params to internal variables. Skip rows skips the Column title.
+        self.S11_arg = loadtxt(open(path_str+"S11_ang_rad.csv", "rb"), delimiter=",", skiprows=1)
+        self.S12_arg = loadtxt(open(path_str+"S12_ang_rad.csv", "rb"), delimiter=",", skiprows=1)
+        self.S21_arg = loadtxt(open(path_str+"S21_ang_rad.csv", "rb"), delimiter=",", skiprows=1)
+        self.S22_arg = loadtxt(open(path_str+"S22_ang_rad.csv", "rb"), delimiter=",", skiprows=1)
 
         #Fetching Frequency data from variable.
-        self.freq = [s11[0] for s11 in self.S11]
+        self.freq = [s11[0] for s11 in self.S11_mag]
 
         #Fetching S param Data from internal variables.
-        self.s11 = [s11[1] for s11 in self.S11]
-        self.s12 = [s12[1] for s12 in self.S12]
-        self.s21 = [s21[1] for s21 in self.S21]
-        self.s22 = [s22[1] for s22 in self.S22]
+        for i in range(0, len(self.S11_mag)):
+            self.s11.append( self.S11_mag[i][1]*(e**(1j*self.S11_arg[i][1])))
+            self.s12.append( self.S12_mag[i][1]*(e**(1j*self.S12_arg[i][1])))
+            self.s21.append( self.S21_mag[i][1]*(e**(1j*self.S21_arg[i][1])))
+            self.s22.append( self.S22_mag[i][1]*(e**(1j*self.S22_arg[i][1])))
 
     def to_db(self, x):
         '''Converts the passed Array to corresponding Db values for Graphical Analysis.'''
@@ -51,7 +63,7 @@ class pycom:
         
         temp = list()
         for i in range(0, len(s11)):
-            val = (sqrt(abs(s11[i]+s12[i]*exp(1j*theta))**2 +(abs(s21[i] + s22[i]*exp(1j*theta))**2)))/self.denominator
+            val = (sqrt(abs(s11[i]+s12[i]*exp(1j*theta))**2 +(abs(s21[i] + s22[i]*exp(1j*theta))**2)))/sqrt(2)
             temp.append(val)
         return(temp)
 
@@ -77,7 +89,9 @@ class pycom:
             self.MEG1.clear()
 
         for i in range(0, len(self.s12)):
-            self.MEG1.append((0.5*(1 - abs(self.s11[i]**2) - abs(self.s12[i])**2))/40)
+            self.MEG1.append((0.5*(1 - abs(self.s11[i]**2) - abs(self.s12[i])**2)))
+        
+        return(self.freq, self.MEG1)
 
     def MEG2_compute(self, clear_first = True):
         '''Computes the MEG2 Value over the provided S Param Values.'''
@@ -86,7 +100,7 @@ class pycom:
             self.MEG2.clear()
 
         for i in range(0, len(self.s12)):
-            self.MEG2.append((0.5*(1 - abs(self.s12[i]**2) - abs(self.s22)[i]**2))/40)
+            self.MEG2.append((0.5*(1 - abs(self.s12[i]**2) - abs(self.s22)[i]**2)))
         return(self.freq, self.MEG2)
 
     def MEG_diff_compute(self, clear_first = True):
@@ -101,24 +115,22 @@ class pycom:
 
         self.MEG_diff = [self.MEG1[i] - self.MEG2[i] for i in range(0, len(self.MEG1))]
         op_values = [
-            {'label' : "MEG1", 'x' : self.freq, 'y' : self.MEG1},
-            {'label' : "MEG2", 'x' : self.freq, 'y' : self.MEG2},
-            {'label' : "MEG1 - MEG2", 'x' : self.freq, 'y' : self.MEG_diff}
+            {'label' : "MEG1", 'x' : self.freq, 'y' : self.to_db(self.MEG1)},
+            {'label' : "MEG2", 'x' : self.freq, 'y' : self.to_db(self.MEG2)},
+            {'label' : "MEG1 - MEG2", 'x' : self.freq, 'y' : self.to_db(self.MEG_diff)}
         ]
 
         return op_values
 
     def ECC_compute(self, clear_first = True):
         '''Computes the Envelope Correlation Coefficient over the provided S Param Values.'''
-        temp = list()
         if(clear_first):
             self.ECC.clear()
 
         for i in range(0, len(self.s12)):
-            val = ( (abs(conj(self.s11[i])*self.s12[i] + conj(self.s21[i])*self.s22[i])**(2)) / ( ( 1 - abs(self.s11[i])**(2) - abs(self.s21[i])**(2)) * (1 - abs(self.s22[i])**(2) - abs(self.s12[i])**(2)) ) )
-            temp.append(val)
-        self.ECC = temp
-        op_values = [{'label' : "ECC", 'x' : self.freq, 'y' : self.to_db(self.ECC)}]
+            self.ECC.append((abs(conj(self.s11[i])*self.s12[i]+conj(self.s21[i])*self.s22[i]))**2/((1-abs(self.s11[i])**2-abs(self.s21[i])**2)*(1-abs(self.s22[i])**2-abs(self.s12[i])**2)))
+
+        op_values = [{'label' : "ECC", 'x' : self.freq, 'y' : self.ECC}]
         return op_values
     
     def DG_compute(self, clear_first = True):
@@ -127,11 +139,15 @@ class pycom:
         if(clear_first):
             self.DG.clear()
 
+        if(len(self.ECC) == 0):
+            self.ECC_compute()
+
         for i in range(0, len(self.s12)):
-            val = 10 * sqrt(1 - ( (abs(self.s11[i]*self.s12[i] + self.s21[i]*self.s22[i])**2) / ( ( 1 - abs(self.s11[i])**2 - abs(self.s21[i])**2) * (1 - abs(self.s22[i])**2 - abs(self.s12[i])**2) ) )**2 )
-            temp.append(val)
+            # val = 10 * sqrt(1 - ( (abs(self.s11[i]*self.s12[i] + self.s21[i]*self.s22[i])**2) / ( ( 1 - abs(self.s11[i])**2 - abs(self.s21[i])**2) * (1 - abs(self.s22[i])**2 - abs(self.s12[i])**2) ) )**2 )
+            # temp.append(val)
+            temp.append(10*sqrt(1 - self.ECC[i]**2))
         self.DG = temp
-        op_values = [{'label' : "DG", 'x' : self.freq, 'y' : self.to_db(self.DG)}]
+        op_values = [{'label' : "DG", 'x' : self.freq, 'y' : self.DG}]
         return op_values
 
     def CCL_compute(self, clear_first = True):
@@ -144,8 +160,10 @@ class pycom:
             val = -1*log( (1 - (abs(self.s11[i])**(2) + abs(self.s11[i])**(2)))*(1 - (abs(self.s22[i])**(2) + abs(self.s22[i])**(2))) - ( -1*((conj(self.s11[i])*self.s12[i]) + (conj(self.s21[i])*self.s12[i])) )*( -1*((conj(self.s11[i])*self.s21[i]) + (conj(self.s12[i])*self.s21[i])) ), 2)
             temp.append(val)
         self.CCL = temp
-
-        op_values = [{'label' : "CCL", 'x' : self.freq, 'y' : self.CCL}]        
+        #savetxt("CCL.csv", self.freq, self.CCL, delimiter=',')
+        
+        op_values = [{'label' : "CCL", 'x' : self.freq, 'y' : self.CCL}] 
+        print(type(self.CCL))      
         return op_values
 
     method_token = {
@@ -156,7 +174,7 @@ class pycom:
         "CCL"  : CCL_compute
     }
 
-    def output(self, property = "TARC"):
+    def output(self, property = "TARC", save = True):
 
         """Generates the Output for the desired property.
 
@@ -175,4 +193,7 @@ class pycom:
             ax.plot(op['x'], real(op['y']), label=op['label'])
         ax.legend()
         ax.set_title(property)
-        plt.show()
+        if save == False:
+            plt.show()
+        else:
+            plt.savefig(f"Output/{property}.png")
